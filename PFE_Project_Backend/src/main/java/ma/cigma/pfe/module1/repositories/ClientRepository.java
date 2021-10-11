911cd72aaf90faddc23d9cab84cfc1e8176125bb
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2021-10-11 15:14:02 +0100

    Update ClientRepository.java
    changed done

--- PFE_Project_Backend/src/main/java/ma/cigma/pfe/module1/repositories/ClientRepository.java
+++ PFE_Project_Backend/src/main/java/ma/cigma/pfe/module1/repositories/ClientRepository.java
@@ -4,10 +4,7 @@ import ma.cigma.pfe.module1.dto.ClientEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * @author Sofia BOUAGRA
- * @project PFE_Project
- */
+
 
 @Repository
 public interface ClientRepository extends CrudRepository<ClientEntity,Integer> {
