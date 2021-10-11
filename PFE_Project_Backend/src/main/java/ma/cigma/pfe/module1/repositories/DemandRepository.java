88946371066cb54d19222d83b69502c3ec4cdab6
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2021-10-11 15:14:33 +0100

    Update DemandRepository.java
    done

--- PFE_Project_Backend/src/main/java/ma/cigma/pfe/module1/repositories/DemandRepository.java
+++ PFE_Project_Backend/src/main/java/ma/cigma/pfe/module1/repositories/DemandRepository.java
@@ -4,11 +4,6 @@ import ma.cigma.pfe.module1.dto.DemandEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * @author Sofia BOUAGRA
- * @project PFE_Project
- */
-
 @Repository
 public interface DemandRepository extends CrudRepository<DemandEntity,Integer> {
 
