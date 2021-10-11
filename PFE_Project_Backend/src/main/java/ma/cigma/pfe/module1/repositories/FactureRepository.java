42b0b3623827042e779eb94488eab3f1b9f40b50
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2021-10-11 15:15:13 +0100

    done

--- PFE_Project_Backend/src/main/java/ma/cigma/pfe/module1/repositories/FactureRepository.java
+++ PFE_Project_Backend/src/main/java/ma/cigma/pfe/module1/repositories/FactureRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * @author Sofia BOUAGRA
+ * @author Brahim Ezzyouy
  * @project PFE_SpringBoot
  */
 
